45aac97103402ef1b270071ff893723d7170ed34
Melissa Goad <mszoopers@protonmail.com>
2020-02-04 11:12:02 -0600

    update

--- xv2.cc
+++ xv2.cc
@@ -48,6 +48,11 @@ u16 r16(const u8 *data)
   return sw16(*(const u16 *)data);
 }
 
+u32 r24(const u8 *data)
+{
+  return (data[0] << 16) | (data[1] << 8) | data[2];
+}
+
 s32 r32s(const u8 *data)
 {
   return sw32(*(const u32 *)data);
@@ -87,18 +92,55 @@ int main(int argc, char **argv)
   u32 size;
   file_load(argv[1], &data, &size);
 
-  u32 adr = 0x1f004;
+  static const char *regs[8] = { "r0", "r1", "r2", "r3", "r4", "r5", "sp", "lnk" };
+
+  u32 adr = 0;
   while(adr < 0x58eba) {
     u32 oadr = adr;
     u8 opcode = r8(data + adr++);
     std::string opc = "?";
 
     switch(opcode) {
-    case 0x06: adr+=2; break;
+    case 0x06: case 0x07: {
+      u32 value = r24(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 22);
+      value &= 0x3fffff;
+      adr += 3;
+      opc = util::string_format("%s = %06x", regs[reg], value);
+      break;
+    }
+    case 0x08: {
+      u32 value = r24(data + adr);
+      adr += 3;
+      opc = util::string_format("jmp.far %06x", value);
+      break;
+    }
     case 0x09: {
-      u32 a1 = (r8(data + adr) << 16) | r16(data + adr + 1);
+      u32 a1 = r24(data + adr);
       adr += 3;
       opc = util::string_format("jsr.far %06x", a1);
+      break;
+    }
+    case 0x0a: case 0xb: {
+      u32 value = r24(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 22);
+      value &= 0x3fffff;
+      adr += 3;
+      opc = util::string_format("%s &= %06x", regs[reg], value);
+      break;
+    }
+    case 0x0c: case 0xd: {
+      u32 value = r24(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 22);
+      value &= 0x3fffff;
+      adr += 3;
+      opc = util::string_format("%s |= %06x", regs[reg], value);
+      break;
+    }
+    case 0x20: {
+      u32 a1 = (adr & 0xffff0000) | r16(data + adr);
+      adr += 2;
+      opc = util::string_format("jmp %06x", a1);
       break;
     }
     case 0x21: {
@@ -107,49 +149,236 @@ int main(int argc, char **argv)
       opc = util::string_format("jsr %06x", a1);
       break;
     }
-    case 0x22: {
-      u32 a1 = adr + r16(data + adr + 1) - 1;
-      adr += 3;
-      opc = util::string_format("branch.cond(?) %04x", a1);
+    case 0x22: case 0x23: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x0000", regs[reg], value << 2);
       break;
     }
-    case 0x23: {
-      u32 a1 = adr + r16(data + adr + 1) - 1;
-      adr += 3;
-      opc = util::string_format("branch.cond(?) %04x", a1);
+    case 0x26: case 0x27: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("cmp %s, %04x", regs[reg], value);
       break;
     }
+    case 0x2a: case 0x2b: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      u8 reg2 = (value >> 11) & 7;
+      value &= 0x7ff;
+      adr += 2;
+      opc = util::string_format("%s = %s & %04x", regs[reg], regs[reg2], value);
+      break;
+    }
+    case 0x2c: case 0x2d: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      u8 reg2 = (value >> 11) & 7;
+      value &= 0x7ff;
+      adr += 2;
+      opc = util::string_format("%s = %s | %04x", regs[reg], regs[reg2], value);
+      break;
+    }
+
     case 0x42: adr+=2; break;
     case 0x44: adr+=2; break;
-    case 0x50: adr+=2; break;
-    case 0x52: adr+=2; break;
-    case 0x56: adr+=2; break;
-    case 0x58: adr+=2; break;
-    case 0x5a: adr+=2; break;
-    case 0x5c: adr+=2; break;
-    case 0x5e: adr+=2; break;
-    case 0x60: adr++; break;
-    case 0x61: {
-      u8 value = r8(data + adr++) & 0x3f;
-      opc = util::string_format("add sp, %c%02x", (value & 0x20) ? '-' : '+', value & 0x1f); //is sp just r1?
+    case 0x50: case 0x51: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x.?", regs[reg], value);
       break;
     }
-    case 0x62: adr++; break;
+    case 0x52: case 0x53: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x.b", regs[reg], value);
+      break;
+    }
+    case 0x54: case 0x55: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x.w", regs[reg], value);
+      break;
+    }
+    case 0x56: case 0x57: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x.l", regs[reg], value);
+      break;
+    }
+    case 0x58: case 0x59: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%s = %04x.l?", regs[reg], value);
+      break;
+    }
+    case 0x5a: case 0x5b: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%04x.b = %s", value, regs[reg]);
+      break;
+    }
+    case 0x5c: case 0x5d: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%04x.w = %s", value, regs[reg]);
+      break;
+    }
+    case 0x5e: case 0x5f: {
+      u16 value = r16(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 14);
+      value &= 0x3fff;
+      adr += 2;
+      opc = util::string_format("%04x.l = %s", value, regs[reg]);
+      break;
+    }
+    case 0x60: case 0x61: {
+      u8 value = r8(data + adr++);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      value &= 0x3f;
+
+      if(value & 0x20)
+	opc = util::string_format("%s -= %02x", regs[reg], 0x40 - value);
+      else
+	opc = util::string_format("%s += %02x", regs[reg], value);
+      break;
+    }
+    case 0x62: case 0x63: {
+      u8 value = r8(data + adr++);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      value &= 0x3f;
+
+      if(value & 0x20)
+	opc = util::string_format("%s = -%02x", regs[reg], 0x40 - value);
+      else
+	opc = util::string_format("%s = %02x", regs[reg], value);
+      break;
+    }
+
+    case 0x66: case 0x67: {
+      u8 value = r8(data + adr++);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      value &= 0x3f;
+
+      if(value & 0x20)
+	opc = util::string_format("cmp %s, -%02x", regs[reg], 0x40 - value);
+      else
+	opc = util::string_format("cmp %s, %02x", regs[reg], value);
+      break;
+    }
+
     case 0x6e: adr++; break;
     case 0x78: case 0x79: {
-      u16 reg = ((opcode << 8) | r8(data + adr++)) & 0x1ff;
-      opc = util::string_format("restore %03x", reg);
+      u8 value = r8(data + adr++);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      value &= 0x3f;
+
+      if(value & 0x20)
+	opc = util::string_format("%s = -%02x[sp].l", regs[reg], 0x40 - value);
+      else
+	opc = util::string_format("%s = %02x[sp].l", regs[reg], value);
       break;
     }
     case 0x7e: case 0x7f: {
-      u16 reg = ((opcode << 8) | r8(data + adr++)) & 0x1ff;
-      opc = util::string_format("save %03x", reg);
+      u8 value = r8(data + adr++);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      value &= 0x3f;
+
+      if(value & 0x20)
+	opc = util::string_format("-%02x[sp].l = %s", 0x40 - value, regs[reg]);
+      else
+	opc = util::string_format("%02x[sp].l = %s", value, regs[reg]);
       break;
     }
-    case 0xa2: adr++; break;
-    case 0xa3: adr++; break;
-    case 0xe0: opc = "rts2"; break;
-    case 0xe1: opc = "rts"; break;
+    case 0x90: case 0x91: {
+      u16 value = r8(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      u8 reg2 = (value >> 3) & 7;
+      adr++;
+      opc = util::string_format("%s = (%s).b", regs[reg], regs[reg2]);
+      break;
+    }
+
+    case 0x98: case 0x99: {
+      u16 value = r8(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      u8 reg2 = (value >> 3) & 7;
+      adr++;
+      opc = util::string_format("%s = (%s).l", regs[reg], regs[reg2]);
+      break;
+    }
+
+    case 0x9e: case 0x9f: {
+      u16 value = r8(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      u8 reg2 = (value >> 3) & 7;
+      adr++;
+      opc = util::string_format("(%s).%x = l", regs[reg2], regs[reg]);
+      break;
+    }
+
+    case 0xa2: case 0xa3: {
+      u16 value = r8(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      u8 reg2 = (value >> 3) & 7;
+      adr++;
+      opc = util::string_format("%s = %s", regs[reg], regs[reg2]);
+      break;
+    }
+
+    case 0xa6: case 0xa7: {
+      u16 value = r8(data + adr);
+      u8 reg = ((opcode & 1) << 2) | (value >> 6);
+      u8 reg2 = (value >> 3) & 7;
+      adr++;
+      opc = util::string_format("cmp %s, %s", regs[reg], regs[reg2]);
+      break;
+    }
+
+    case 0xc8: adr++; break;
+    case 0xca: adr++; break;
+    case 0xd2: {
+      s8 value = r8(data + adr++);
+      opc = util::string_format("b?? %06x", adr - 2 + value);
+      break;
+    }
+    case 0xd5: {
+      s8 value = r8(data + adr++);
+      opc = util::string_format("b?? %06x", adr - 2 + value);
+      break;
+    }
+    case 0xd7: {
+      s8 value = r8(data + adr++);
+      opc = util::string_format("b?? %06x", adr - 2 + value);
+      break;
+    }
+    case 0xda: {
+      s8 value = r8(data + adr++);
+      opc = util::string_format("bne %06x", adr - 2 + value);
+      break;
+    }
+
+    case 0xe0: opc = "jmp (lnk)"; break;
+    case 0xe1: opc = "jmp (a)"; break;
+    case 0xe3: opc = "rti"; break;
     }
     printf("%06x:", oadr);
     for(u32 a = oadr; a < adr; a++)
